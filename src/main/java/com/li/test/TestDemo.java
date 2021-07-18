5b74a07484822edea412de1fee2f8b851c3ecc3e
li022721 <li022721@126.com>
2021-07-18 22:54:22 +0800

    测试

---
+++ src/main/java/com/li/test/TestDemo.java
@@ -0,0 +1,11 @@
+package com.li.test;
+
+/**
+ * @author li
+ * @create 2021-07-18 20:29
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+        System.out.println("hello github");
+    }
+}
